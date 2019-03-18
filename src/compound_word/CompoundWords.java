d015a7dcfc558e554357d7ec17d372559ef2e240
Michael Johnson <mike@mikejfromva.com>
2019-03-18 17:13:51 -0400

    Implemented a O(L^2 * log(N)) solution

--- src/compound_word/CompoundWords.java
+++ src/compound_word/CompoundWords.java
@@ -1,6 +1,7 @@
 package compound_word;
 
 import java.util.List;
+import java.util.TreeSet;
 
 /**
  * 
@@ -11,18 +12,28 @@ import java.util.List;
  */
 public class CompoundWords {
 	public enum Implementation {
-		SLOW;
+		SLOW, FAST;
 	}
 
 	public static boolean isCompoundWord(String word, List<String> dictionary, Implementation method) {
 		switch (method) {
 		case SLOW:
 			return slow(word, dictionary);
+		case FAST:
+			return fast(word, dictionary);
 		default:
 			throw new RuntimeException("Java should provid exhaustive enum switches");
 		}
 	}
 
+	/**
+	 * Assuming word is L letters and dictionary has N entries. N is large compared
+	 * to L. Time complexity is O(N^L)
+	 * 
+	 * @param word
+	 * @param dictionary
+	 * @return
+	 */
 	private static boolean slow(String word, List<String> dictionary) {
 		if (word.equals(""))
 			return true;
@@ -34,6 +45,35 @@ public class CompoundWords {
 			if (word.substring(0, prefix.length()).equals(prefix)) {
 				if (slow(word.substring(prefix.length()), dictionary))
 					return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Assuming word is L letters and dictionary has N entries. N is large compared
+	 * to L Time complexity is O(L^2*log(N)).
+	 * 
+	 * @param word
+	 * @param dictionary
+	 * @return
+	 */
+	private static boolean fast(String word, List<String> dictionary) {
+		TreeSet<String> treeSet = new TreeSet<String>(dictionary);
+		return fast(word, treeSet);
+	}
+
+	private static boolean fast(String word, TreeSet<String> dictionary) {
+		if (word.equals(""))
+			return true;
+		for (int start = 0; start < word.length(); start++) {
+			for (int finish = start + 1; finish < word.length() + 1; finish++) {
+				if (dictionary.contains(word.substring(start, finish))) {
+					if (finish == word.length())
+						return true;
+					if (fast(word.substring(finish, word.length()), dictionary))
+						return true;
+				}
 			}
 		}
 		return false;
