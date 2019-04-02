d5f8bfb74b381e02dd25965800cc33553cba0da8
Michael Johnson <mike@mikejfromva.com>
2019-04-02 06:40:56 -0400

    shuffle corrected syntax errors

--- src/shuffle/Shuffle.java
+++ src/shuffle/Shuffle.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 public class Shuffle {
 
-	public static T void shuffle(List<T> deck) {
-		  for (int i=0; i<deck.length; i++) {
+	public static <T> void shuffle(List<T> deck) {
+		  for (int i=0; i<deck.size(); i++) {
 		    // swap
-		    int slot = random() * deck.length;
+		    int slot = (int)(Math.random() * (double) deck.size());
 		    T tmp1 = deck.get(slot);
-		    S tmp2 = deck.get(i);
+		    T tmp2 = deck.get(i);
 		    deck.set(i, tmp1);
 		    deck.set(slot, tmp2);
 		  }
