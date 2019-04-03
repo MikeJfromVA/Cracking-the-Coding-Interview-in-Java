6104cc650c5854b7623415412401e14895804664
Michael Johnson <mike@mikejfromva.com>
2019-04-03 19:19:28 -0400

    set to array

--- src/SelectRandom.java
+++ src/SelectRandom.java
@@ -1,35 +1,54 @@
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 
+ * A method to randomly generate a set of m integers from an array of n integers.
+ * 
+ * My first attempt at this problem had me reading the word 'set' quite literally. I have
+ * since changed it to return an array of integers.
+ * 
+ * @author michaeljohnson
+ *
+ */
 public class SelectRandom {
 
-	public static Set<Integer> selectRandom(int[] array, int m) {
+	public static int[] selectRandom(int[] array, int m) {
+		array = array.clone(); // O(n) for defensive copy
+		int[] results = new int[m];
 		int remaining = array.length;
-		Set<Integer> set = new HashSet<>();
-		for (int i = 0; i < m; i++) { // O(m)
+		for (int i = 0; i < m; i++) {
 			int selectedIndex = (int) (Math.random() * remaining);
-			set.add(array[selectedIndex]);
+			results[i] = array[selectedIndex];
 			array[selectedIndex] = array[remaining - 1];
 			remaining--;
 		}
-		return set;
+		return results;
 	}
 
+	public static void printArray(int[] array) {
+		for (int e: array) {
+			System.out.print(e);
+			System.out.print(" ");
+		}
+		System.out.println();
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(selectRandom(new int[] { 1, 2, 3, 4 }, 0));
-		System.out.println(selectRandom(new int[] { 1, 2, 3, 4 }, 1));
-		System.out.println(selectRandom(new int[] { 1, 2, 3, 4 }, 2));
-		System.out.println(selectRandom(new int[] { 1, 2, 3, 4 }, 3));
-		System.out.println(selectRandom(new int[] { 1, 2, 3, 4 }, 4));
-		System.out.println(selectRandom(new int[] { 1, 2, 3, 4 }, 0));
-		System.out.println(selectRandom(new int[] { 1, 2, 3, 4 }, 1));
-		System.out.println(selectRandom(new int[] { 1, 2, 3, 4 }, 2));
-		System.out.println(selectRandom(new int[] { 1, 2, 3, 4 }, 3));
-		System.out.println(selectRandom(new int[] { 1, 2, 3, 4 }, 4));
-		System.out.println(selectRandom(new int[] { 1, 2, 3, 4 }, 0));
-		System.out.println(selectRandom(new int[] { 1, 2, 3, 4 }, 1));
-		System.out.println(selectRandom(new int[] { 1, 2, 3, 4 }, 2));
-		System.out.println(selectRandom(new int[] { 1, 2, 3, 4 }, 3));
-		System.out.println(selectRandom(new int[] { 1, 2, 3, 4 }, 4));
+		printArray(selectRandom(new int[] { 1, 2, 3, 4 }, 0));
+		printArray(selectRandom(new int[] { 1, 2, 3, 4 }, 1));
+		printArray(selectRandom(new int[] { 1, 2, 3, 4 }, 2));
+		printArray(selectRandom(new int[] { 1, 2, 3, 4 }, 3));
+		printArray(selectRandom(new int[] { 1, 2, 3, 4 }, 4));
+		printArray(selectRandom(new int[] { 1, 2, 3, 4 }, 0));
+		printArray(selectRandom(new int[] { 1, 2, 3, 4 }, 1));
+		printArray(selectRandom(new int[] { 1, 2, 3, 4 }, 2));
+		printArray(selectRandom(new int[] { 1, 2, 3, 4 }, 3));
+		printArray(selectRandom(new int[] { 1, 2, 3, 4 }, 4));
+		printArray(selectRandom(new int[] { 1, 2, 3, 4 }, 0));
+		printArray(selectRandom(new int[] { 1, 2, 3, 4 }, 1));
+		printArray(selectRandom(new int[] { 1, 2, 3, 4 }, 2));
+		printArray(selectRandom(new int[] { 1, 2, 3, 4 }, 3));
+		printArray(selectRandom(new int[] { 1, 2, 3, 4 }, 4));
 	}
 }
